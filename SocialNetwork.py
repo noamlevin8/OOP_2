181849cc7d7949b5fcdd4b862b4f836f73d3eece
noamlevin8 <noamlevin11@gmail.com>
2024-02-11 13:29:22 +0200

    first commit

--- SocialNetwork.py
+++ SocialNetwork.py
@@ -1,3 +1,6 @@
+from User import User
+
+
 class SocialNetwork():
 
     # Singleton class constractor
@@ -5,13 +8,33 @@ class SocialNetwork():
         if not hasattr(cls, 'instance'):
             cls.instance = super(SocialNetwork, cls).__new__()
             cls.instance.name = name
+            cls.instance.user_list = []
         return cls.instance
 
     def sign_up(self, name, password):
+        used = False
+        for index, user in enumerate(self.user_list):
+            if user.name == name:
+                used = True
+                break
+        if not used:
+            if len(password) >= 4 and len(password) <= 8:
+                user = User(name, password)
+                self.user_list.append(user)
 
     def log_in(self, name, password):
+        for index, user in enumerate(self.user_list):
+            if user.name == name:
+                if user.password == password:
+                    user.connection(True)
+                # incorrect password
+                break
 
     def log_out(self, name):
+        for index, user in enumerate(self.user_list):
+            if user.name == name:
+                user.connection(False)
+                break
 
     def __str__(self):
         return "Social Network"
--- User.py
+++ User.py
@@ -2,21 +2,29 @@ class User():
     def __init__(self, name, password):
         self.name = name
         self.password = password
+        self.following = []
+        self.connected = True
 
     def follow(self, other):
-        print(f"{self.name} started following {other.name}")
+        if other.name not in self.following:
+            self.following.append(other.name)
+            print(f"{self.name} started following {other.name}")
 
     def unfollow(self, other):
-        print(f"{self.name} unfollowed {other.name}")
+        if other.name in self.following:
+            self.following.remove(other.name)
+            print(f"{self.name} unfollowed {other.name}")
 
     #needs change
     def print_notifications(self):
         print("notification")
 
-    def __connection(self, state):
-        if state == True:
+    def connection(self, state):
+        if state == True and self.connected == False:
+            self.connected = True
             print("connected")
-        else:
+        elif state == False and self.connected == True:
+            self.connected = False
             print("disconnected")
 
     def __str__(self):
