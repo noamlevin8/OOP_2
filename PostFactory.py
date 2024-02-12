9f4f89763d53854efc1166ae39c8a7660fdf99ee
noamlevin8 <noamlevin11@gmail.com>
2024-02-12 20:53:33 +0200

    first commit

---
+++ PostFactory.py
@@ -0,0 +1,13 @@
+from ImagePost import ImagePost
+from SalePost import SalePost
+from TextPost import TextPost
+
+class PostFactory():
+    @staticmethod
+    def create_post(post_type, owner, content, price=None, loc=None):
+        if post_type == "Text":
+            return TextPost(owner, content)
+        elif post_type == "Image":
+            return ImagePost(owner, content)
+        elif post_type == "Sale":
+            return SalePost(owner, content, price, loc)
